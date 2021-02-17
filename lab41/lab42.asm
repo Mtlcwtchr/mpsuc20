09d8bf4206933e463779bb19e2c48eb6e5e69191
Mtlcwtchr <aliquaminimicus@gmail.com>
2021-02-18 01:20:28 +0300

    4.2-lab updated and guideline added

--- lab41/lab42.asm
+++ lab41/lab42.asm
@@ -1,8 +1,3 @@
-;In clear timer on compare or CTC mode (WGM02:0 = 2), the OCR0A register is used to manipulate the counter resolution. In
-;CTC mode the counter is cleared to zero when the counter value (TCNT0) matches the OCR0A. The OCR0A defines the top
-;value for the counter, hence also its resolution.
-
-
 .include "m328pdef.inc"
 
 .cseg
@@ -17,20 +12,18 @@ Reset:
 	ldi R16, High(RAMEND)
 	out SPH, R16
 
-	ldi R16, 0b00000001
+	ldi R16, 0b00000011
 	sts TIMSK0, r16
 	sei
 
 	ldi R20, 0x00
 	out TCNT0, R20
-	;configure timer (CTC MODE: WGM02=1) 1.1
-	ldi R16, 0b00001101 ; CS02=1, CS01=0, CS00=1 => clc/1024 ~ 62500t/sec => 1 timer clock ~ 16mks
+	;configure timer (CTC MODE: WGM01=1) 1.1
+	ldi R16, 0b00000010
+	out TCCR0A, R16
+	ldi R16, 0b00000101
 	out TCCR0B, R16
-	;configure max TCNT0 value 
-	ldi r16, 0x7D ;overflow after 125 clock ~ 8ms
-	out OCR0A, r16
-	ldi r16, 0x00
-	out OCR0B, r16
+
 	;configure output
 	LDI R16, 0b11110011
 	OUT DDRD, R16
@@ -39,25 +32,23 @@ Reset:
 	LDI R16, 0b00000000
 	OUT DDRC, R16
 
-	ldi r16, 0b10101010
-	ldi r17, 0b00001010
-
 	ldi r18, 0b01010101
 
+clear_cmprs:
 	clr r20
-
-off:
-	clr r19
 	clr r21
+clear_counter:
+	clr r19
 main:
+	;if none pressed or pressed both skip
 	IN R23, PINC
 	CPI R23, 0b00000000
-	breq off
+	breq clear_cmprs
 	CPI R23, 0b00110000
-	breq off
-
+	breq clear_cmprs
+delay:
 	cp R19, r21
-	brne main
+	brne delay
 
 	IN R23, PINC
 	CPI R23, 0b00010000
@@ -74,6 +65,80 @@ s2:
 	RJMP print
 
 print:
+	;required cause INT0 and INT1 are clear and output shifted
+	rcall modify_out
+
+	OUT PORTD, R24
+	OUT PORTB, R25
+
+	rjmp clear_counter
+
+
+s1_proceed:
+	;check if s1-pressed-case-flag set
+	cpi r22, 0b00000001
+	breq skip_s1_set
+	;set output to default for s1-pressed-case
+	ldi r16, 0b00000000
+	ldi r17, 0b00001000
+	;set s1-pressed-case-flag
+	ldi r22, 0b00000001
+
+	;configure max TCNT0 value 
+	ldi r16, 0x7C
+	out OCR0A, r16
+
+	;clear timer and delay counter, set count of skipping overflows to required for s1-pressed-case
+	clr r19
+	out TCNT0, R20
+	ldi r21, 0x19
+
+	;skip 1 iteration
+	rjmp ret_s1
+skip_s1_set:
+	;shift right high
+	ror r17
+	;shift right low
+	ror r16
+	brcc ret_s1
+	ror r17
+	;shift to output
+	ror r17
+	ror r17
+	ror r17
+	ror r17
+ret_s1:	ret
+
+
+s2_proceed:
+	;check if s2-pressed-case-flag set
+	cpi r22, 0b00000010
+	;if set skip setting
+	breq skip_s2_set
+	;set output to default for s2-pressed-case
+	ldi r16, 0b10101010
+	ldi r17, 0b00001010
+	;set s2-pressed-case-flag
+	ldi r22, 0b00000010
+
+	;configure max TCNT0 value 
+	ldi r16, 0x96
+	out OCR0A, r16
+
+	;clear timer and delay counter, set count of skipping overflows to required for s2-pressed-case
+	clr r19
+	out TCNT0, R20
+	ldi r21, 0x77
+
+	;skip 1 iteration
+	rjmp ret_s2
+skip_s2_set:
+	;apply invert mask
+	eor r16, r18
+	eor r17, r18
+ret_s2:	ret
+
+modify_out:
 	mov r24, r16
 	mov r25, r17
 
@@ -88,49 +153,10 @@ print:
 	lsr r26
 	lsr r26
 	or r24, r26
-
-	OUT PORTD, R24
-	OUT PORTB, R25
-
-	clr r19
-	rjmp main
+ret_modify_out:	ret
 
 
 TIMER0_OVF:
 	inc R19
 	out TCNT0, R20
 	reti
-
-
-s1_proceed:
-	ldi r21, 0x19
-	clr r19
-	brbs 6, skip_s1_set
-	ldi r16, 0b00000000
-	ldi r17, 0b00001000
-	bset 6
-	rjmp ret_s1
-skip_s1_set:
-	ror r17
-	ror r16
-	brcc ret_s1
-	ror r17
-	ror r17
-	ror r17
-	ror r17
-	ror r17
-ret_s1:	ret
-
-
-s2_proceed:
-	ldi r21, 0x90
-	clr r19
-	brbc 6, skip_s2_set
-	ldi r16, 0b10101010
-	ldi r17, 0b00001010
-	bclr 6
-	rjmp ret_s2
-skip_s2_set:
-	eor r16, r18
-	eor r17, r18
-ret_s2:	ret
